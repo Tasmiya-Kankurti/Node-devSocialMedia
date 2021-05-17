a4b8753841d405ec6ae8a623bc18a03762eb5343
Tasmiya Kankurti <tasmiyakankurti@gmail.com>
2021-05-17 14:01:11 +0530

    profile schema added

---
+++ src/models/Profile.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const profileSchema = new Schema({
+    career: {
+        type: String,
+        required: true
+    },
+
+    work: {
+        type: String,
+        required: true 
+    },
+
+    companyWebsite: {
+        type: String,
+        required: false 
+    },
+
+    citystate: {
+        type: String,
+        required: false 
+    },
+
+    skills: {
+        type: String,
+        required: true 
+    },
+
+    gitName: {
+        type: String,
+        required: false
+    },
+
+    about: {
+        type: String,
+        required: false 
+    },
+
+    twitter: {
+        type: String,
+        required: false
+    },
+
+    facebook: {
+        type: String,
+        required: false
+    },
+
+    youtube: {
+        type: String,
+        required: false 
+    },
+
+    linkedIn: {
+        type: String,
+        required: false
+    },
+
+    Instagram: {
+        type: String,
+        required: false
+    }
+}) 
+
+const profile = mongoose.model("profile", profileSchema)
+module.exports = profile
